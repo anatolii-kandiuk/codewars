e5671ce8c28ada94dd0ed26eed856891533ba53b
anatolii <batkovich327@gmail.com>
2023-02-17 18:27:17 +0200

    Kata 6ku - Data compression using run-length encoding

--- src/main/java/org/codewars/Main.java
+++ src/main/java/org/codewars/Main.java
@@ -3,6 +3,7 @@ package org.codewars;
 import org.codewars.bitcoinStatistics.Stats;
 import org.codewars.goodVsEvil.GoodVsEvil;
 import org.codewars.mexicanWave.MexicanWave;
+import org.codewars.runLengthEncoding.RunLengthEncoding;
 import org.codewars.uniqueNumber.Kata;
 
 import java.util.Arrays;
@@ -12,7 +13,11 @@ public class Main {
 
         System.out.println("Hello Codewars!");
 
-        System.out.println(GoodVsEvil.battle("8448 6448 7289 619 9043 6719", "6441 9532 4865 1292 1492 4375 9228"));
+        String encode = RunLengthEncoding.encode("KKKAAAAANNNNNNDDDDDDIIIIIIIUUUUUUKKKKKK");
+        System.out.println(encode);
+        System.out.println(RunLengthEncoding.decode(encode));
+
+//        System.out.println(GoodVsEvil.battle("8448 6448 7289 619 9043 6719", "6441 9532 4865 1292 1492 4375 9228"));
 //        Kata kata = new Kata();
 //
 //        System.out.println(kata.findUniq(new double[]{1, 1, 1, 2, 1, 1}));
---
+++ src/main/java/org/codewars/runLengthEncoding/RunLengthEncoding.java
@@ -0,0 +1,44 @@
+package org.codewars.runLengthEncoding;
+
+import static java.util.regex.Pattern.compile;
+
+public class RunLengthEncoding {
+    public static String encode(String input) {
+
+        StringBuilder encodeData = new StringBuilder();
+
+        int counterLetter = 1;
+
+        for(int i = 0; i < input.length(); i++) {
+
+            if (i < input.length() - 1 && input.charAt(i) == input.charAt(i+1)) {
+                counterLetter++;
+                i++;
+            }
+            encodeData.append(counterLetter).append(input.charAt(i));
+            counterLetter = 1;
+        }
+
+        return encodeData.toString();
+
+        //  return compile("(.)\\1*").matcher(input).replaceAll(m -> m.end() - m.start() + m.group(1));
+    }
+
+    public static String decode(String input) {
+
+        String[] decodeArray;
+
+        String regex = "(?<=\\D)(?=\\d)|(?<=\\d)(?=\\D)";
+
+        decodeArray = input.split(regex);
+
+        for(int i = 1; i < decodeArray.length; i += 2) {
+            decodeArray[i] = decodeArray[i].repeat(Integer.parseInt(decodeArray[i-1]));
+            decodeArray[i-1] = "";
+        }
+
+        return String.join("",  decodeArray);
+
+        //  return compile("(\\d+)(\\D)").matcher(input).replaceAll(m -> m.group(2).repeat(Integer.parseInt(m.group(1))));
+    }
+}
