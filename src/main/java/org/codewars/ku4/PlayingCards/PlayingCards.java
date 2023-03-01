bcf584a79883b2af58fd582d94b1535d250e172c
anatolii <batkovich327@gmail.com>
2023-03-01 18:53:20 +0200

    Kata 4ku - Hide a message in a deck of playing cards

---
+++ src/main/java/org/codewars/ku4/PlayingCards/PlayingCards.java
@@ -0,0 +1,24 @@
+package org.codewars.ku4.PlayingCards;
+
+public class PlayingCards {
+    /**
+     * Takes a String containing a message, and returns an array of Strings representing
+     * a deck of playing cards ordered to hide the message
+     */
+    public String[] encode(String message) {
+        String[] deck = new String[] {};
+
+        return deck;
+    }
+
+    /**
+     * Takes an array of Strings representing a deck of playing cards, and returns
+     * the message that is hidden inside
+     */
+    public String decode(String[] deck) {
+        // PlayingCardsOutput.printDeck(deck, true); // Using unicode characters
+        // PlayingCardsOutput.printDeck(deck, false); // Using regular characters
+
+        return "";
+    }
+}
